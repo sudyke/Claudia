98ffeec6d94631b3b2b6a2d1c8a6ce65d4d2241f
sudyke <sudyke@gmail.com>
2026-05-26 20:26:34 +0200

    Initial Commit

---
+++ Claudia/ClaudiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudiaApp.swift
+//  Claudia
+//
+//  Created by Steve Sudy on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
